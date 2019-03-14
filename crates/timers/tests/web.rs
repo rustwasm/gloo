81f774716ffb3ecd51ef2121bf9c40b6b99c4360
Joey Hain <jmhain@protonmail.com>
2019-03-13 18:24:42 -0700

    Add setInterval support to `gloo_timers`

--- crates/timers/tests/web.rs
+++ crates/timers/tests/web.rs
@@ -3,7 +3,7 @@
 #![cfg(target_arch = "wasm32")]
 
 use futures::prelude::*;
-use gloo_timers::{Timeout, TimeoutFuture};
+use gloo_timers::{Interval, IntervalStream, Timeout, TimeoutFuture};
 use std::cell::Cell;
 use std::rc::Rc;
 use wasm_bindgen_test::*;
@@ -79,5 +79,58 @@ fn timeout_future_cancel() -> impl Future<Item = (), Error = wasm_bindgen::JsVal
         .map(move |_| {
             // We should never have fired `b`'s timer.
             assert_eq!(cell.get(), false);
+        })
+}
+
+#[wasm_bindgen_test(async)]
+fn interval() -> impl Future<Item = (), Error = wasm_bindgen::JsValue> {
+    let (mut sender, receiver) = futures::sync::mpsc::channel(1);
+    Interval::new(1, move || sender.try_send(()).unwrap()).forget();
+    receiver
+        .take(5)
+        .map_err(|_| "impossible".into())
+        .for_each(|_| Ok(()))
+}
+
+#[wasm_bindgen_test(async)]
+fn interval_cancel() -> impl Future<Item = (), Error = wasm_bindgen::JsValue> {
+    let cell = Rc::new(Cell::new(false));
+
+    let i = Interval::new(1, {
+        let cell = cell.clone();
+        move || {
+            cell.set(true);
+            panic!("should have been cancelled");
+        }
+    });
+    i.cancel();
+
+    let (sender, receiver) = futures::sync::oneshot::channel();
+
+    Interval::new(2, move || {
+        sender.send(()).unwrap();
+        assert_eq!(cell.get(), false);
+    })
+    .forget();
+
+    receiver.map_err(|e| e.to_string().into())
+}
+
+#[wasm_bindgen_test(async)]
+fn interval_stream() -> impl Future<Item = (), Error = wasm_bindgen::JsValue> {
+    let cell = Rc::new(Cell::new(0));
+    IntervalStream::new(1)
+        .take(5)
+        .map_err(|_| "impossible".into())
+        .for_each({
+            let cell = cell.clone();
+            move |_| {
+                cell.set(cell.get() + 1);
+                Ok(())
+            }
+        })
+        .and_then(move |_| {
+            assert_eq!(cell.get(), 5);
+            Ok(())
         })
 }
