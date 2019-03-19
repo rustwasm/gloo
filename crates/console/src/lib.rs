8d0798239c4facbd5663d24ade3447677154dd67
Christian Brevik <christian.brevik@gmail.com>
2019-03-19 09:59:17 +0100

    Add static scope method to ConsoleTimer

--- crates/console/src/lib.rs
+++ crates/console/src/lib.rs
@@ -28,11 +28,30 @@ impl<'a> ConsoleTimer<'a> {
     /// ```no_run
     /// use gloo_console::ConsoleTimer;
     ///
-    /// let _timer = ConsoleTimer::new("my_label");
+    /// let _timer = ConsoleTimer::new("foo");
     /// ```
     pub fn new(label: &'a str) -> ConsoleTimer<'a> {
         console::time_with_label(label);
         ConsoleTimer { label }
+    }
+
+    /// Starts a scoped console time measurement
+    ///
+    /// # Example
+    ///
+    /// ```no_run
+    /// use gloo_console::ConsoleTimer;
+    ///
+    /// let value = ConsoleTimer::scope("foo", || {
+    ///     // ...
+    /// });
+    /// ```
+    pub fn scope<F, T>(label: &str, f: F) -> T
+    where
+        F: FnOnce() -> T,
+    {
+        let _timer = ConsoleTimer::new(label);
+        f()
     }
 }
 
@@ -40,12 +59,4 @@ impl<'a> Drop for ConsoleTimer<'a> {
     fn drop(&mut self) {
         console::time_end_with_label(self.label);
     }
-}
-
-#[macro_export]
-macro_rules! console_time {
-    ($t:tt, $b:block) => {
-        let _timer = ConsoleTimer::new($t);
-        $b
-    };
 }
