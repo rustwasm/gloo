2dec6ed8eb816855f82aa214bdf26ea4206e728c
Ryan Levick <ryan.levick@gmail.com>
2019-03-27 17:14:08 +0100

    Correct indentation

--- crates/file/src/file.rs
+++ crates/file/src/file.rs
@@ -1,9 +1,9 @@
 pub struct File {
-  pub(crate) inner: web_sys::File,
+    pub(crate) inner: web_sys::File,
 }
 
 impl File {
-  pub fn from_raw(inner: web_sys::File) -> File {
-    File { inner }
-  }
+    pub fn from_raw(inner: web_sys::File) -> File {
+        File { inner }
+    }
 }
--- crates/file/src/file_reader.rs
+++ crates/file/src/file_reader.rs
@@ -6,29 +6,32 @@ use wasm_bindgen::JsCast;
 use crate::blob::Blob;
 
 pub struct FileReader {
-  inner: web_sys::FileReader,
+    inner: web_sys::FileReader,
 }
 
 impl FileReader {
-  pub fn new() -> FileReader {
-    FileReader {
-      inner: web_sys::FileReader::new().unwrap(),
+    pub fn new() -> FileReader {
+        FileReader {
+            inner: web_sys::FileReader::new().unwrap(),
+        }
     }
-  }
 
-  pub fn read_as_string(self, blob: &impl Blob) -> impl futures::Future<Item = String, Error = ()> {
-    let (tx, rx) = futures::sync::oneshot::channel();
-    let reader = Rc::new(self.inner);
-    let cloned_reader = reader.clone();
-    let cb = wasm_bindgen::closure::Closure::once_into_js(move || {
-      let _ = cloned_reader.result().map(|r| {
-        let _ = tx.send(r.as_string().unwrap());
-      });
-    });
-    let reader = reader.clone();
-    let function = cb.dyn_into().unwrap();
-    reader.set_onload(Some(&function));
-    reader.read_as_text(&blob.raw()).unwrap();
-    rx.map_err(|_| ())
-  }
+    pub fn read_as_string(
+        self,
+        blob: &impl Blob,
+    ) -> impl futures::Future<Item = String, Error = ()> {
+        let (tx, rx) = futures::sync::oneshot::channel();
+        let reader = Rc::new(self.inner);
+        let cloned_reader = reader.clone();
+        let cb = wasm_bindgen::closure::Closure::once_into_js(move || {
+            let _ = cloned_reader.result().map(|r| {
+                let _ = tx.send(r.as_string().unwrap());
+            });
+        });
+        let reader = reader.clone();
+        let function = cb.dyn_into().unwrap();
+        reader.set_onload(Some(&function));
+        reader.read_as_text(&blob.raw()).unwrap();
+        rx.map_err(|_| ())
+    }
 }
