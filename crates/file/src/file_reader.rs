e79f19b321be087e2a4fd93d5defa0b6e41daa67
Ryan Levick <ryan.levick@gmail.com>
2019-03-28 16:57:46 +0100

    ReadAsString future

--- crates/file/src/file_reader.rs
+++ crates/file/src/file_reader.rs
@@ -1,12 +1,19 @@
-use std::rc::Rc;
-
-use futures::Future;
+use futures::sync::oneshot;
+use futures::{Async, Future};
+use wasm_bindgen::closure::Closure;
 use wasm_bindgen::JsCast;
+use wasm_bindgen::UnwrapThrowExt;
 
 use crate::blob::Blob;
 
+#[derive(Debug)]
 pub struct FileReader {
     inner: web_sys::FileReader,
+}
+
+pub struct ReadAsString {
+    receiver: oneshot::Receiver<String>,
+    _closure: Closure<FnMut()>,
 }
 
 impl FileReader {
@@ -16,22 +23,43 @@ impl FileReader {
         }
     }
 
-    pub fn read_as_string(
-        self,
-        blob: &impl Blob,
-    ) -> impl futures::Future<Item = String, Error = ()> {
+    pub fn read_as_string(self, blob: &impl Blob) -> ReadAsString {
         let (tx, rx) = futures::sync::oneshot::channel();
-        let reader = Rc::new(self.inner);
-        let cloned_reader = reader.clone();
-        let cb = wasm_bindgen::closure::Closure::once_into_js(move || {
-            let _ = cloned_reader.result().map(|r| {
-                let _ = tx.send(r.as_string().unwrap());
+        let reader = self.inner.clone();
+        let closure = Closure::once(move || {
+            let _ = reader.result().map(|js_string| {
+                let _ = tx.send(js_string.as_string().unwrap_throw());
             });
         });
-        let reader = reader.clone();
-        let function = cb.dyn_into().unwrap();
-        reader.set_onload(Some(&function));
-        reader.read_as_text(&blob.raw()).unwrap();
-        rx.map_err(|_| ())
+        let function = closure.as_ref().dyn_ref().unwrap_throw();
+        self.inner.clone().set_onload(Some(&function));
+        self.inner.read_as_text(&blob.raw()).unwrap_throw();
+        ReadAsString {
+            receiver: rx,
+            _closure: closure,
+        }
+    }
+}
+
+impl Future for ReadAsString {
+    type Item = String;
+    type Error = ();
+
+    fn poll(&mut self) -> Result<Async<Self::Item>, Self::Error> {
+        match self.receiver.poll() {
+            Ok(Async::Ready(value)) => Ok(Async::Ready(value)),
+            Ok(Async::NotReady) => Ok(Async::NotReady),
+            Err(_) => Err(()),
+        }
+    }
+}
+
+// TODO: remove when wasm-bindgen#1387 is fixed
+impl std::fmt::Debug for ReadAsString {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        f.debug_struct("EventListener")
+            .field("receiver", &self.receiver)
+            .field("callback", &"Closure { ... }")
+            .finish()
     }
 }
