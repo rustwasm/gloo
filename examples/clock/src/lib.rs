ee8a02b8a98a093fff50d22bb1a5520105119935
Richard Dodd <richard.o.dodd@gmail.com>
2019-12-05 16:02:14 +0000

    Add styling to example

--- examples/clock/src/lib.rs
+++ examples/clock/src/lib.rs
@@ -7,11 +7,14 @@ use chrono::Timelike;
 #[wasm_bindgen(start)]
 pub fn main() {
     console_error_panic_hook::set_once();
+
     let document = web_sys::window().unwrap_throw().document().unwrap_throw();
+
     let el = document.get_element_by_id("clock").unwrap_throw();
 
     // render the date, then set it to re-render every second.
     render_date(&el);
+
     spawn_local(async move {
         IntervalStream::new(1_000).for_each(|_| {
             render_date(&el);
@@ -24,11 +27,13 @@ pub fn main() {
 fn render_date(el: &web_sys::Element) {
     // print the current date
     let date = chrono::Local::now();
+
     let format_str = if date.second() % 2 == 0 {
         "%Y-%m-%d %H %M"
     } else {
         "%Y-%m-%d %H:%M"
     };
+
     let date_str = date.format(format_str).to_string();
 
     // Set the contents of `el` to our date string
