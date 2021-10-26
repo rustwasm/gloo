5b67085faa49d46eff083be1e87f6e4bc7176a88
Jonas Platte <jplatte@users.noreply.github.com>
2021-10-26 17:10:42 +0200

    utils: Add body() and document_element() getters (#161)

--- crates/utils/src/lib.rs
+++ crates/utils/src/lib.rs
@@ -11,6 +11,18 @@ pub fn document() -> web_sys::Document {
     window().document().expect_throw("Can't find document")
 }
 
+/// Convenience function to access `document.body`.
+pub fn body() -> web_sys::HtmlElement {
+    document().body().expect_throw("Can't find document body")
+}
+
+/// Convenience function to access `document.documentElement`.
+pub fn document_element() -> web_sys::Element {
+    document()
+        .document_element()
+        .expect_throw("Can't find document element")
+}
+
 /// Convenience function to access the web_sys history.
 pub fn history() -> web_sys::History {
     window().history().expect_throw("Can't find history")
