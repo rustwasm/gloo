2f4590165944e12ba9e353adfae234fe295ee6e3
Muhammad Hamza <muhammadhamza1311@gmail.com>
2021-11-25 22:00:36 +0500

    Add console_dbg macro (#170)
    * add console_dbg macro
    
    * fmt

--- crates/console/src/console_dbg.rs
+++ crates/console/src/console_dbg.rs
@@ -1,7 +1,8 @@
 /// A macro similar to [`dbg!`].
 ///
 /// See the [stdlib documentation][std::dbg] to learn more. This macro calls `console.log`
-/// instead of `eprintln!`
+/// instead of `eprintln!` for `JsValue`s. The formatting is done by the browser. If you want
+/// [`Debug`][std::fmt::Debug] implementation to be used instead, consider using [`console_dbg`]
 #[macro_export]
 macro_rules! console {
     () => {
@@ -18,6 +19,65 @@ macro_rules! console {
         }
     };
     ($($val:expr),+ $(,)?) => {
-        ($($crate::dbg!($val)),+,)
+        ($($crate::console!($val)),+,)
     };
+}
+
+/// A macro similar to [`dbg!`].
+///
+/// See the [stdlib documentation][std::dbg] to learn more. This macro calls `console.log`
+/// instead of `eprintln!`. This macro passing the values to [`console`] after formatting them using
+/// the [`Debug`][std::fmt::Debug] implementation.
+#[macro_export]
+macro_rules! console_dbg {
+    () => {
+        $crate::console!()
+    };
+    ($val:expr $(,)?) => {
+        match $val {
+            v => {
+                $crate::console!(::std::format!("{:?}", v));
+                v
+            }
+        }
+    };
+    ($($val:expr),+ $(,)?) => {
+        ($($crate::console_dbg!($val)),+,)
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    #![allow(dead_code)]
+    //! These exist to ensure code compiles
+    use wasm_bindgen::JsValue;
+
+    fn console_works() {
+        console!();
+        {
+            let js_value = JsValue::from("test");
+            console!(js_value);
+        }
+        {
+            let js_value_1 = JsValue::from("test 1");
+            let js_value_2 = JsValue::from("test 2");
+            console!(js_value_1, js_value_2);
+        }
+    }
+
+    fn console_dbg_works() {
+        #[derive(Debug)]
+        struct Value(&'static str);
+
+        console_dbg!();
+        {
+            let value = Value("test");
+            console_dbg!(value);
+        }
+        {
+            let value_1 = Value("test 1");
+            let value_2 = Value("test 2");
+            console_dbg!(value_1, value_2);
+        }
+    }
 }
