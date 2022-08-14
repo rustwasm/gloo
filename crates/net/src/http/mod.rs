ca72234e0d9218b8f6c5b48b5bd3a4af21950c75
Andrey Snow <148123+andoriyu@users.noreply.github.com>
2022-08-14 13:17:50 -0700

    fix: Break dependency cycle by not using serde-serialize (#239)

--- crates/net/src/http/mod.rs
+++ crates/net/src/http/mod.rs
@@ -391,9 +391,16 @@ impl Response {
     #[cfg_attr(docsrs, doc(cfg(feature = "json")))]
     pub async fn json<T: DeserializeOwned>(&self) -> Result<T, Error> {
         let promise = self.response.json().map_err(js_to_error)?;
-        let json = JsFuture::from(promise).await.map_err(js_to_error)?;
+        let json = JsFuture::from(promise)
+            .await
+            .map_err(js_to_error)
+            .and_then(|json| {
+                js_sys::JSON::stringify(&json)
+                    .map(String::from)
+                    .map_err(js_to_error)
+            })?;
 
-        Ok(json.into_serde()?)
+        Ok(serde_json::from_str(&json)?)
     }
 
     /// Reads the response as a String.
