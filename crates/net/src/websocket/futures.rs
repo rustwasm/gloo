7252b1c3cb15ac64448125a73c4c493289ce7fe2
Muhammad Hamza <muhammadhamza1311@gmail.com>
2022-08-17 21:05:50 +0500

    fmt

--- crates/net/src/websocket/futures.rs
+++ crates/net/src/websocket/futures.rs
@@ -99,7 +99,7 @@ impl WebSocket {
     /// [MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/API/WebSocket/WebSocket#exceptions_thrown)
     /// to learn more.
     ///
-    /// This function requires `json` features because protocols are parsed by `serde` into `JsValue`. 
+    /// This function requires `json` features because protocols are parsed by `serde` into `JsValue`.
     #[cfg_attr(docsrs, doc(cfg(feature = "json")))]
     #[cfg(feature = "json")]
     pub fn open_with_protocols<S: AsRef<str> + serde::Serialize>(
