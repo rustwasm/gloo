6f37fe3fdb135a4b041c8e7bddaee4bed7a68156
Kaede Hoshikawa <futursolo@icloud.com>
2022-09-05 21:44:53 +0900

    Fix clippy warnings.

--- crates/history/tests/hash_history.rs
+++ crates/history/tests/hash_history.rs
@@ -1,6 +1,3 @@
-use std::time::Duration;
-
-use gloo_timers::future::sleep;
 use wasm_bindgen_test::{wasm_bindgen_test as test, wasm_bindgen_test_configure};
 
 use gloo_history::{HashHistory, History};
