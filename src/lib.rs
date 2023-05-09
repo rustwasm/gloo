3fa2f873782315f5f8a7b4370340dc1dc9d16bd4
Patrick Elsen <pelsen@xfbs.net>
2023-05-09 17:49:00 -0400

    Modularizes gloo.
    This turns the individual subcrates into optional features. By default,
    all features are enabled, to ensure this change is not a breaking
    change. But if needed, default features can be turned off and only the
    modules needed enabled.

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,25 @@
 #![deny(missing_docs, missing_debug_implementations)]
 
 // Re-exports of toolkit crates.
+#[cfg(feature = "console")]
 pub use gloo_console as console;
+#[cfg(feature = "dialogs")]
 pub use gloo_dialogs as dialogs;
+#[cfg(feature = "events")]
 pub use gloo_events as events;
+#[cfg(feature = "file")]
 pub use gloo_file as file;
+#[cfg(feature = "history")]
 pub use gloo_history as history;
+#[cfg(feature = "net")]
 pub use gloo_net as net;
+#[cfg(feature = "render")]
 pub use gloo_render as render;
+#[cfg(feature = "storage")]
 pub use gloo_storage as storage;
+#[cfg(feature = "timers")]
 pub use gloo_timers as timers;
+#[cfg(feature = "utils")]
 pub use gloo_utils as utils;
+#[cfg(feature = "worker")]
 pub use gloo_worker as worker;
