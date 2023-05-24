d6debc73ff95c18f40ffc6f7dd520728a52c0269
Charles Schleich <c.schleich@dr-flex.de>
2023-05-24 16:44:47 +0200

    Add Eq and PartialEq derives for eventsource::State

--- crates/net/src/eventsource/mod.rs
+++ crates/net/src/eventsource/mod.rs
@@ -11,7 +11,7 @@ use std::fmt;
 ///
 /// See [`EventSource.readyState` on MDN](https://developer.mozilla.org/en-US/docs/Web/API/EventSource/readyState)
 /// to learn more.
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum State {
     /// The connection has not yet been established.
     Connecting,
